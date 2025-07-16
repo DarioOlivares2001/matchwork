702cd4c95b69b68c42112a03eb3a85b43cacc516
DarioOlivares2001 <olivaresdario2001@gmail.com>
2025-07-16 10:15:45 -0400

    Fix: update production environment URLs for API and WebSocket

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,13 +1,13 @@
-export const environment = {
+/*export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080/api',
   wsUrl: 'ws://localhost:8080/ws'
-};
+};*/
 
 
 
-/*export const environment = {
+export const environment = {
   production: false,
   apiBaseUrl: 'https://ponkybonk.com/api',
   wsUrl: 'wss://ponkybonk.com/ws' 
-};*/
+};
